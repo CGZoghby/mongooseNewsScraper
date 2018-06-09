8b536b3405cada53330c2e081f0b95e328379e7b
Christopher Gregory Zoghby <cgzoghby@gmail.com>
2018-06-09 12:42:58 -0400

    Scrape Only Through API Path

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get("/scrape", function (req, res) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
-        // Now, we grab every h2 within an article tag, and do the following:
+        // Now, we grab every h3 within an article tag, and do the following:
         $(".content-card-content h3").each(function (i, element) {
             // Save an empty result object
             var result = {};
